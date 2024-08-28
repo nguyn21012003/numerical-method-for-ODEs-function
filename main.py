eb284ca4ebf6f3fb9b46737ea89a342f93332661
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-08-28 12:36:00 +0700

    fix some bugs

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def promt_user(id):
         case 9:
             a = save_solution("solution")
         case _:
-            raise TypeError("TypeError")
+            sys.exit(TypeError("TypeError"))
 
 
 def open_csv_file(name):
@@ -92,7 +92,7 @@ def initialValue():
                 const, value = input(f"Input value of x_{i}(eg: x0=1,x1=2,...): ").strip().lower().split("=")
                 initValue[const] = value
                 i += 1
-            except EOFError as e:
+            except EOFError:
                 print(initValue)
                 break
             except KeyboardInterrupt as kbi:
@@ -140,7 +140,6 @@ def true_solution(f, tspan, y0, h):
     t = np.arange(tspan[0], tspan[1] + h, h)
     y = np.zeros(len(t))
     y[0] = y0
-
     for n in range(len(t)):
         y[n] = f(t[n])
 
@@ -152,8 +151,8 @@ def local_error(epsilon):
 
 
 # Create ODE function
-# name = input("Input the function f(t,y): ").replace("^", "**")
-def ode_function(t, y):
+# The function must be in the quote and unquote. eg: "t^2-y^2"
+def ode_function(t: float, y: float) -> float:
     """Define an initial value problem (mean the problem function for eg: y'=t*y)
 
     Args:
@@ -163,12 +162,16 @@ def ode_function(t, y):
     Returns:
         _float_: _description_
     """
+
     # Input is obeyed by the rule y' = f(t,y). Just only need input the f(t,y)
-    f = lambda t, y: eval(sys.argv[1].replace("^", "**").replace(" ", ""))
+    def f(t: float, y: float) -> float:
+        e = eval(sys.argv[1].replace("^", "**").replace(" ", ""))
+        return e
+
     return f(t, y)
 
 
-def tspan_init(args):
+def tspan_init(args: str) -> tuple:
     """_summary_
     Input the string for eg [1,2], return a tuple type.
     Args:
@@ -193,11 +196,11 @@ def hstep():
 
 
 # Solvers
-def euler_method(f, tn, yn, h):
+def euler_method(f: float, tn: list, yn: list, h: float) -> float:
     return yn + h * f(tn, yn)
 
 
-def rk4_method(f, tn, yn, h):
+def rk4_method(f: float, tn: list, yn: list, h: float) -> float:
     k1 = f(tn, yn)
     k2 = f(tn + 0.5 * h, yn + 0.5 * h * k1)
     k3 = f(tn + 0.5 * h, yn + 0.5 * h * k2)
@@ -210,7 +213,7 @@ def bisection_method(f, tn, yn, h):
 
 
 # Solve the ODE function
-def solveODEs(f, tspan, y0, h, solver):
+def solveODEs(f: float, tspan: list, y0: float, h: float, solver: float) -> float:
     """_summary_
 
     Args:
@@ -218,7 +221,7 @@ def solveODEs(f, tspan, y0, h, solver):
         tspan (list): Is the interval of t from t0 to tend
         y0 (float): Description
         h (float): is the step that calculate from t0 to t1. This can get from the hstep() function
-        solver (type): is any solver like Euler Method or RK4 Method
+        solver (float): is any solver like Euler Method or RK4 Method
     """
     t = np.arange(tspan[0], tspan[1] + h, h)
     y = np.zeros(len(t))  # Create an array with all the array[i] is zero
@@ -229,24 +232,13 @@ def solveODEs(f, tspan, y0, h, solver):
     return t, y
 
 
-def save_solution(name):
+def save_solution(name: str):
 
     match name:
         case "euler":
             solver = euler_method
             namefile = "solution_" + name + ".csv"
-
             t, y = solveODEs(ode_function, tspan_init(input("Input the interval of t (eg: [0,1]): ")), y0_init(), hstep(), solver)
-            with open(namefile, "w", newline="") as writefile:
-                header = ["t", "y"]
-                writer = csv.DictWriter(writefile, fieldnames=header)
-                writer.writeheader()
-                for i in range(len(t)):
-                    writer.writerow({"t": f"{float(t[i]):2.3f}", "y": f"{float(y[i])}"})
-
-        case "solution":
-            namefile = name + ".csv"
-            t, y = true_solution(true_solution_function, tspan_init(input("Input the interval of t (eg: [0,1]): ")), y0_init(), hstep())
             with open(namefile, "w", newline="") as writefile:
                 header = ["t", "y"]
                 writer = csv.DictWriter(writefile, fieldnames=header)
@@ -257,6 +249,15 @@ def save_solution(name):
             solver = rk4_method
             namefile = "solution_" + name + ".csv"
             t, y = solveODEs(ode_function, tspan_init(input("Input the interval of t (eg: [0,1]): ")), y0_init(), hstep(), solver)
+            with open(namefile, "w", newline="") as writefile:
+                header = ["t", "y"]
+                writer = csv.DictWriter(writefile, fieldnames=header)
+                writer.writeheader()
+                for i in range(len(t)):
+                    writer.writerow({"t": f"{float(t[i]):2.3f}", "y": f"{float(y[i])}"})
+        case "solution":
+            namefile = name + ".csv"
+            t, y = true_solution(true_solution_function, tspan_init(input("Input the interval of t (eg: [0,1]): ")), y0_init(), hstep())
             with open(namefile, "w", newline="") as writefile:
                 header = ["t", "y"]
                 writer = csv.DictWriter(writefile, fieldnames=header)
